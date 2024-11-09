4e538e61ca361043930950e78a75c6163a2d9eb4
Vlad Lautaru <vlad.lautaru@student.upt.ro>
2024-11-09 15:13:58 +0200

    Basic answer handle

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,8 +1,16 @@
 import React from "react";
 import '../css/Welcome.css';
+import {Link} from "react-router-dom";
 
 function Welcome () {
-    return;
+    return(
+        <div>
+            <h1> Welcome </h1>
+            <Link to='/questions'>
+                <button>Question</button>
+            </Link>
+        </div>
+    );
 }
 
 export default Welcome;
