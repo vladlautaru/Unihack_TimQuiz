dbd32afd7ad58469070fbddcba560ff664c575a0
vladlautaru <vlad.lautaru@student.upt.ro>
2024-11-10 11:25:57 +0200

    Fixes for Question and Welcome

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,94 +1,148 @@
-import React, {useState} from "react";
-import '../css/Question.css';
+import React, {useState} from 'react';
 import {useNavigate} from "react-router-dom";
+import '../css/Question.css'
+import ChatBox from './ChatBox'
 
-const questions = [
+const questions=[
     {
-        text: "What is 16th of Deember associated with in Timisoara?",
-        answers: [
-            {
-                text: "Protests against OUG 13",
-                correct: false
-            },
-            {
-                text: "The 1989 Revolution",
-                correct: true
-            },
-            {
-                text: "The 1907 Peasant Uprising",
-                correct: false
-            },
-            {
-                text: "Ending of the First World War",
-                correct: false
-            }
+        question: "Ce râu curge prin Timișoara?",
+        answers:[
+            {text:"Timișul", correct:false},
+            {text:"Bega", correct:true},
+            {text:"Crișul", correct:false},
+            {text:"Mureșul", correct:false},
         ]
     },
     {
-        text: "Which of the following inventors was born in Timisoara?",
-        answers: [
-            {
-                text: "Louis Pasteur",
-                correct: false
-            },
-            {
-                text: "Petrache Poenaru",
-                correct: false
-            },
-            {
-                text: "James Watt",
-                correct: false
-            },
-            {
-                text: "Francesco Illy",
-                correct: true
-            }
+        question: "Ce loc este prezentat in imagine?",
+        answers:[
+            {text:"Piața Libertății", correct:false},
+            {text:"Piața Victoriei", correct:false},
+            {text:"Bastionul Maria Theresia", correct:false},
+            {text:"Piața Unirii", correct:true},
+        ],
+        image: require('../images/piata_unirii_intrebare.jpg')
+    },
+
+    {
+        question: "Timișoara drept capitală europeană a culturii a avut sloganul:",
+        answers:[
+            {text:"Luminează orașul prin tine", correct:true},
+            {text:"Luminează și strălucește alături de ceilalți", correct:false},
+            {text:"Fă lumină în jurul tău", correct:false},
+            {text:"Tu ești lumina orașului", correct:false},
         ]
+    },
+    {
+        question: "Ce semnifică ziua de 16 Decembrie pentru Timișoara?",
+        answers:[
+            {text:"Dezastrul de la Colectiv", correct:false},
+            {text:"Revoluția din 1989", correct:true},
+            {text:"Revolta din 1907", correct:false},
+            {text:"Încheierea Primulul Război Mondial", correct:false}
+        ]
+    },
+    {
+        question: "Care dintre următorii inventatori a fost născut în Timișoara?",
+        answers:[
+            {text:"Aurel Vlaicu", correct:false},
+            {text:"Henri Coandă", correct:false},
+            {text:"Victor Babeș", correct:false},
+            {text:"Francesco Illy", correct: true}
+        ]
+    },
+    {
+        question: "Care este numele cladirii prezentate in imagine?",
+        answers:[
+            {text:"Palatul Culturii", correct:false},
+            {text:"Catedrala Mitropolitana Ortodoxa Romana", correct:false},
+            {text:"Sinagoga din Fabric", correct:true},
+            {text:"Palatul Apelor", correct:false}
+        ],
+        image: require('../images/sinagoga_fabric.jpg')
     }
 ];
 
-function Question () {
+function Questions() {
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-    const [currentScore, setCurrentScore] = useState(0);
-    const [answers, setAnswers] = useState(Array(questions.length));
-
+    const [score, setScore] = useState(0);
+    const [answers, setAnswers] = useState(Array(questions.length).fill(false));
     const navigate = useNavigate();
 
-    const handleAnswer = (correct) => {
+    const handleAnswer = (isCorrect) => {
         const nextQuestionIndex = currentQuestionIndex + 1;
-        const nextScore = currentScore + (correct) ? 10 : 0;
+        const currentScore = score + ((isCorrect) ? 10 : 0);
 
-        setAnswers((prevAnswers) => {
+        setAnswers(prevAnswers => {
             const updatedAnswers = [...prevAnswers];
-            updatedAnswers[currentQuestionIndex] = correct;
+            updatedAnswers[currentQuestionIndex] = isCorrect;
             return updatedAnswers;
         });
 
-        setCurrentScore(nextScore);
+        setScore(currentScore);
 
         if (nextQuestionIndex < questions.length) {
             setCurrentQuestionIndex(nextQuestionIndex);
+        } else {
+            navigate('/score', { state: {score: currentScore}});
         }
-
-        console.log("score: " + currentScore);
-        console.log("question:" + currentQuestionIndex);
-        console.log("array: " + answers.toString());
     }
 
-    return(
-        <div>
-            <h1>TimQuiz</h1>
-            <h2>{questions[currentQuestionIndex].text}</h2>
-            <button onClick={() => handleAnswer(questions[currentQuestionIndex].answers[0].correct)}>
-                {questions[currentQuestionIndex].answers[0].text}</button>
-            <button onClick={() => handleAnswer(questions[currentQuestionIndex].answers[1].correct)}>
-                {questions[currentQuestionIndex].answers[1].text}</button>
-            <button onClick={() => handleAnswer(questions[currentQuestionIndex].answers[2].correct)}>
-                {questions[currentQuestionIndex].answers[2].text}</button>
-            <button onClick={() => handleAnswer(questions[currentQuestionIndex].answers[3].correct)}>
-                {questions[currentQuestionIndex].answers[3].text}</button>
+    const goToNext = () => {
+        if (currentQuestionIndex < questions.length - 1) {
+            setCurrentQuestionIndex(currentQuestionIndex + 1);
+        } else {
+            navigate('/score', { state: { score } });
+        }
+    }
+
+    const goToPrevious = () => {
+        if (currentQuestionIndex > 0) {
+            const prevIndex = currentQuestionIndex - 1;
+            const wasCorrect = answers[prevIndex];
+
+            if (wasCorrect) {
+                setScore(score - 10);
+            }
+
+            setAnswers(prevAnswers => {
+                const updatedAnswers = [...prevAnswers];
+                updatedAnswers[prevIndex] = false;
+                return updatedAnswers;
+            });
+
+            setCurrentQuestionIndex(currentQuestionIndex - 1);
+        } else {
+            navigate('/');
+        }
+    }
+
+    const hasImage = questions[currentQuestionIndex].image;
+    return (
+        <div className="app">
+            <div className="title">
+                <h1>TimQuiz</h1>
+            </div>
+            <div className={`quiz ${hasImage ? "question-with-image" : "question-no-image"}`}>
+                <h2 id="question">{questions[currentQuestionIndex].question}</h2>
+                {hasImage && (
+                    <img src={questions[currentQuestionIndex].image} className="imagines"/>
+                )}
+                <div id="answer" className={questions[currentQuestionIndex].image ? "answer-with-image" : "answer-no-image"}>
+                    {questions[currentQuestionIndex].answers.map((answer, index) => (
+                        <button key={index} className="btn" onClick={() => handleAnswer(answer.correct)}>
+                            {answer.text}
+                        </button>
+                    ))}
+                </div>
+                <div className="button-container">
+                    <button id="previous-btn" onClick={goToPrevious}>Previous</button>
+                    <button id="next-btn" onClick={goToNext}>Next</button>
+                </div>
+            </div>
+            <ChatBox />
         </div>
     );
 }
 
-export default Question;
+export default Questions;
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,29 +1,34 @@
-
 import React from 'react';
 import '../css/Welcome.css';
-
+import {Link} from "react-router-dom";
 
 function Welcome() {
     return (
         <div className="container">
+            <div className="logo">
+                <link rel="stylesheet" href="css/style.css"/>
+            </div>
             <div className="main-menu">
                 <ul>
                     <h1 className="hero-heading"></h1>
                 </ul>
                 <section className="hero">
                     <div className="hero-content">
-                        <h1 className="hero-heading">TimQuiz</h1>
+                        <h1 className="hero-heading" style={{fontSize: '3rem'}}>TimQuiz</h1>
                         <div className="vertical-center">
-                            <button className="button" role="button">Start</button>
+                            <Link to="/questions">
+                                <button className="button" role="button">Start</button>
+                            </Link>
                         </div>
 
-                        <div className="title">TIMISOARA</div>
+                        <div className="letter">TIMISOARA</div>
                         <div className="name">Hai sa descoperim Timisoara!</div>
                         <div className="name2">Esti gata de o noua aventura?</div>
                     </div>
-                    <img src={require('../images/imagine-timisoara2.jpg')} className="image2" alt="Timisoara 2"/>
-                    <img src={require('../images/imagine-timisoara3.jpg')} className="image3" alt="Timisoara 3"/>
+
                 </section>
+                <img src={require('../images/imagine-timisoara2.jpg')} className="image1" alt="Timisoara 1"/>
+                <img src={require('../images/imagine-timisoara3.jpg')} className="image3" alt="Timisoara 3"/>
             </div>
         </div>
     );
